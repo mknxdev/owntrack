e953d862eb609ea19a3c52b1c56460380da21b4a
polo <paulguzdariviere@gmail.com>
2023-07-15 18:02:56 +0200

    Better UI for entry banner

--- src/UIManager.ts
+++ src/UIManager.ts
@@ -3,38 +3,55 @@ export default class UIManager {
   _elRoot: Element
 
   constructor() {
-    this._initRoot()
+    this._initBase()
     this._initEntry()
+    this._initSettingsModal()
     window.addEventListener('DOMContentLoaded', this._mount.bind(this))
   }
 
-  _initRoot() {
+  _initBase() {
     this._elRoot = document.createElement('div')
     this._elRoot.classList.add('ot-root')
   }
 
   _initEntry() {
-    const elBanner = document.createElement('div')
-    elBanner.classList.add('ot-banner')
+    const elEntryWrapper = document.createElement('div')
+    elEntryWrapper.classList.add('ot-entrywrapper')
+    const elEntry = document.createElement('div')
+    elEntry.classList.add('ot-entry')
+    const elEntryNotice = document.createElement('div')
+    elEntryNotice.classList.add('ot-entry__notice')
+    elEntryNotice.innerHTML =
+      '<p>This website uses cookies &amp; analytics for tracking and/or advertising purposes. You can choose to accept them or not.</p>'
+    elEntry.append(elEntryNotice)
     const btns = [
-      { t: 'Allow', c: 'allow', h: this._onAllowAll },
-      { t: 'Deny', c: 'deny', h: this._onDenyAll },
+      { t: 'Settings', c: ['settings', 'ot-ghost'], h: this._onSettingsClick },
+      { t: 'Deny', c: ['deny', 'ot-error'], h: this._onDenyAllClick },
+      { t: 'Allow', c: ['allow', 'ot-success'], h: this._onAllowAllClick },
     ]
+    const elEntryBtns = document.createElement('div')
+    elEntryBtns.classList.add('ot-entry__btns')
     for (const btn of btns) {
-      const elBtn = document.createElement('button')
-      elBtn.classList.add(btn.c)
-      elBtn.innerText = btn.t
-      elBtn.addEventListener('click', btn.h)
-      elBanner.append(elBtn)
+      const elEntryBtn = document.createElement('button')
+      btn.c.forEach((c) => elEntryBtn.classList.add(c))
+      elEntryBtn.innerText = btn.t
+      elEntryBtn.addEventListener('click', btn.h)
+      elEntryBtns.append(elEntryBtn)
     }
-    this._elRoot.append(elBanner)
+    elEntry.append(elEntryBtns)
+    elEntryWrapper.append(elEntry)
+    this._elRoot.append(elEntryWrapper)
   }
 
-  _onAllowAll() {
+  _initSettingsModal() {}
+
+  _onSettingsClick() {}
+
+  _onAllowAllClick() {
     console.log('allowed')
   }
 
-  _onDenyAll() {
+  _onDenyAllClick() {
     console.log('denied')
   }
 
