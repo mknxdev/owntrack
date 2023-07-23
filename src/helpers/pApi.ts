9681c8bf1f306664cf80e808ca0ed18a9415de5a
mekkanix <paulguzdariviere@gmail.com>
2023-07-23 03:38:19 +0200

    Add locales structure

--- src/helpers/pApi.ts
+++ src/helpers/pApi.ts
@@ -1,44 +1,35 @@
-import { Config } from '../types'
+import { Config, ConfigService, Locales, LocaleDefinition } from '../types'
 
 const err = (msg: string) => {
   throw new Error(msg)
 }
 
-export const checkForValidConfig = (config: Config): boolean => {
+const checkServicesConf = (services: ConfigService[]): boolean => {
   try {
-    // config
-    if (!config)
-      err('OwnTrack: A configuration object is required at first call.')
-    // config.enableRequiredCookies
-    if (
-      config.enableRequiredCookies &&
-      typeof config.enableRequiredCookies !== 'boolean'
-    )
-      err(`OwnTrack: 'enableRequiredCookies' must be a boolean.`)
-    // config.services
-    if (!config.services) err(`OwnTrack: 'services' is required.`)
-    if (!Array.isArray(config.services))
+    // services
+    if (!services) err(`OwnTrack: 'services' is required.`)
+    if (!Array.isArray(services))
       err(`OwnTrack: 'services' must be an array.`)
-    // config.services.service
-    if (config.services) {
-      for (const srv of config.services) {
-        // config.services.service.name
+    // services.service
+    if (services) {
+      for (const srv of services) {
+        // services.service.name
         if (!srv.name.trim())
           err(`OwnTrack: Service [${srv.name}]: 'name' is required.`)
-        if (config.services.filter((s) => s.name === srv.name).length > 1)
+        if (services.filter((s) => s.name === srv.name).length > 1)
           err(`OwnTrack: Service names must be unique.`)
-        // config.services.service.label
+        // services.service.label
         if (srv.label && typeof srv.label !== 'string')
           err(`OwnTrack: Service [${srv.name}]: 'label' must be a string.`)
-        // config.services.service.type
+        // services.service.type
         if (srv.type && typeof srv.type !== 'string')
           err(`OwnTrack: Service [${srv.name}]: 'type' must be a string.`)
-        // config.services.service.description
+        // services.service.description
         if (srv.description && typeof srv.description !== 'string')
           err(
             `OwnTrack: Service [${srv.name}]: 'description' must be a string.`,
           )
-        // config.services.service[scripts | onInit | handlers]
+        // services.service[scripts | onInit | handlers]
         if (
           (!srv.scripts || !srv.scripts.length) &&
           !srv.onInit &&
@@ -52,7 +43,7 @@ export const checkForValidConfig = (config: Config): boolean => {
           err(
             `OwnTrack: Service [${srv.name}]: 'handlers' must contain at least one property if 'scripts' and 'onInit' are omitted.`,
           )
-        // config.services.service.scripts
+        // services.service.scripts
         if (srv.scripts && !Array.isArray(srv.scripts))
           err(`OwnTrack: Service [${srv.name}]: 'scripts' must be an array.`)
         if (srv.scripts && srv.scripts.some((s) => !s.url))
@@ -63,10 +54,10 @@ export const checkForValidConfig = (config: Config): boolean => {
           err(
             `OwnTrack: Service [${srv.name}]: 'url' property of 'scripts' elements must be a string.`,
           )
-        // config.services.service.onInit
+        // services.service.onInit
         if (srv.onInit && typeof srv.onInit !== 'function')
           err(`OwnTrack: Service [${srv.name}]: 'onInit' must be a function.`)
-        // config.services.service.handlers
+        // services.service.handlers
         if (srv.handlers && typeof srv.handlers !== 'object')
           err(`OwnTrack: Service [${srv.name}]: 'handlers' must be an object.`)
         if (
@@ -83,6 +74,34 @@ export const checkForValidConfig = (config: Config): boolean => {
     return false
   }
   return true
+}
+
+const checkGlobalConf = (config: Config): boolean => {
+  try {
+    // config
+    if (!config)
+      err('OwnTrack: A configuration object is required at first call.')
+    // config.enableRequiredCookies
+    if (
+      config.enableRequiredCookies &&
+      typeof config.enableRequiredCookies !== 'boolean'
+    )
+      err(`OwnTrack: 'enableRequiredCookies' must be a boolean.`)
+  } catch (err) {
+    console.error(err.message)
+    return false
+  }
+  return true
+}
+
+const checkLocalesConf = (locales: Locales): boolean => {
+  return true
+}
+
+export const checkForValidConfig = (config: Config): boolean => {
+  return checkServicesConf(config.services) &&
+         checkGlobalConf(config) &&
+         checkLocalesConf(config.locales)
 }
 
 export const fillDefaultValues = (config: Config): Config => {
--- src/types.ts
+++ src/types.ts
@@ -1,18 +1,5 @@
 import TrackingService from './TrackingService'
 
-export type ConfigService = {
-  name: string
-  label?: string
-  type?: string
-  description?: string
-  scripts?: { url: string }[]
-  onInit?: Function
-  handlers?: object
-}
-export type Config = {
-  enableRequiredCookies?: boolean
-  services: ConfigService[]
-}
 export type Consent = {
   value: boolean
   reviewed: boolean
@@ -29,4 +16,25 @@ export type TrackingServiceConsent = {
   srv: string
   v: boolean
   r: boolean
+}
+
+export type LocaleDefinition = {}
+export type Locales = {
+  en: LocaleDefinition,
+  fr: LocaleDefinition
+}
+export type ConfigService = {
+  name: string
+  label?: string
+  type?: string
+  description?: string
+  scripts?: { url: string }[]
+  onInit?: Function
+  handlers?: object
+}
+export type Config = {
+  enableRequiredCookies?: boolean
+  services: ConfigService[]
+  locale?: string
+  locales?: Locales
 }
