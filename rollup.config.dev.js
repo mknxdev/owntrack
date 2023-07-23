a325a42fb5e1e52f09cfdeaa6223aad8b85262ca
polo <paulguzdariviere@gmail.com>
2023-07-23 19:38:08 +0200

    Prevent wds to be started from 'build:dev' cmd

--- rollup.config.dev.js
+++ rollup.config.dev.js
@@ -2,6 +2,22 @@ import serve from 'rollup-plugin-serve'
 import typescript from '@rollup/plugin-typescript'
 import css from 'rollup-plugin-import-css'
 
+const plugins = [
+  typescript(),
+  css({
+    output: 'owntrack.css',
+  }),
+]
+if (process.env.MODE === 'local')
+  plugins.splice(
+    0,
+    0,
+    serve({
+      contentBase: 'public',
+      port: 8080,
+    }),
+  )
+
 export default {
   input: 'src/index.ts',
   output: [
@@ -11,14 +27,5 @@ export default {
       format: 'umd',
     },
   ],
-  plugins: [
-    serve({
-      contentBase: 'public',
-      port: 8080,
-    }),
-    typescript(),
-    css({
-      output: 'owntrack.css',
-    }),
-  ],
+  plugins,
 }
